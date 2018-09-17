13e532a17bac8fdc238be2264866f786a712a183
Ronan Bernard <ronan.bernard@smile.fr>
2018-09-17 14:37:29 +0200

    feat - scss - auto compiler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Commands from './components/Commands';
 import Header from './components/Header';
-import './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
